5a4bc1dbac52a85f96d570f32a730e1c1bb81cbe
Andrés Saavedra <afsaavedram@unal.edu.co>
2024-05-10 14:30:08 +0000

    feat: final code added

--- number_guess.sh
+++ number_guess.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PSQL="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"
+PSQL="psql -X --username=freecodecamp --dbname=number_guess --no-align --tuples-only -c"
 
 #READ THE USERNAME
 echo "Enter your username:"
@@ -7,54 +7,75 @@ read USERNAME
 #echo $USERNAME
 
 #SEARCH FOR THE USER IN DATABASE
-USER_FROM_DB=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
-echo $USER_FROM_DB
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+RETURNING_USER=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
+#echo $USER_FROM_DB
+
 #IF NOT EXIST
 
+#SAVE THE NEW NAME AND PROMPT THE WELCOME MESSAGE FOR NEW GAMERS
+if [[ -z $USER_ID ]]
+  then
+    INSERT_NEW_NAME=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+    echo "Welcome, $USERNAME! It looks like this is your first time here."
 
-  #SAVE THE NEW NAME AND PROMPT THE WELCOME MESSAGE FOR NEW GAMERS
+#ELSE, PROMPT THE WELCOME MESSAGE FOR OLD GAMERS, SEARCH TOTAL NUMBER OF GAMES AND FEWST NUMBER OF GUESSES 
+else
+  GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games INNER JOIN users USING(user_id) WHERE user_id=$USER_ID")
+  BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games INNER JOIN users USING(user_id) WHERE user_id =$USER_ID")
+  echo "Welcome back, $RETURNING_USER! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 
+fi
 
-#ELSE, PROMPT THE WELCOME MESSAGE FOR OLD GAMERS
+#GENERATE THE RANDOM BETWEEN (1:1000) NUMBER AND START THE GAME AND READ THE NUMBER
+RANDOM_NUMBER=$((1 + RANDOM % 1000))
+echo "Guess the secret number between 1 and 1000:"
+echo $RANDOM_NUMBER
+read NUMBER
 
-
-#GENERATE THE RANDOM BETWEEN (1:1000) NUMBER AND START THE GAME
-
-
+TRIES=1
 #WHILE RANDOM NUMBER IS DIFFERENT
-
-  #READ THE NUMBER
-
-
-  #IF IS NOT AN INTEGER
-
-
-    #PROMPT THE MESSAGE
-
-
-  #ELSE
-
-
-    #IF RANDOM NUMBER IS HIGHER THAN
-
+while [[ $RANDOM_NUMBER != $NUMBER ]]
+do
   
-      #READ THE NUMBER AGAIN AND SUM 1 TO THE ATTEMPTS
-  
+  #IF IS AN INTEGER
+  if [[ $NUMBER =~ ^[0-9]+$ ]]
+    then
+      #echo "its a number"
+      
+      #IF RANDOM NUMBER IS HIGHER THAN
+      if [[ $NUMBER -lt $RANDOM_NUMBER ]]
+        then
+          echo "It's higher than that, guess again:"
+          
+          #READ THE NUMBER AGAIN AND SUM 1 TO THE ATTEMPTS
+          read NUMBER
+          TRIES=$(( TRIES +1 ))
 
-    #ELIF RANDOM NUMBER IS LOWER THAN
+        #ELIF RANDOM NUMBER IS LOWER THAN
+      elif [[ $NUMBER -gt $RANDOM_NUMBER ]]
+        then
+          echo "It's lower than that, guess again:"
 
-  
-        #READ THE NUMBER AGAIN AND SUM 1 TO THE ATTEMPTS
+          #READ THE NUMBER AGAIN AND SUM 1 TO THE ATTEMPTS
+          read NUMBER
+          TRIES=$(( TRIES +1 ))
 
-
-    #ELIF RANDOM NUMBER IS EQUAL
+      #ELIF RANDOM NUMBER IS EQUAL
 
 
         #SUM 1 TO THE ATTEMPTS AND SAVE THE RECORD
+      fi
+    else
+      echo "That is not an integer, guess again:"
+      read NUMBER
+  fi
 
-
-#END OF WHILE
-
+done
 
 #PRINT THE CONGRATULATIONS MESSAGE
-#
+#NUMBER_OF_GUESSES=$($PSQL "SELECT COUNT(guesses) FROM games INNER JOIN users USING(user_id)  WHERE user_id=$USER_ID")
+
+echo "You guessed it in $TRIES tries. The secret number was $RANDOM_NUMBER. Nice job!"
+
+#END OF WHILE
